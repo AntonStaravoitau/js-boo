17ae3b4384748d519ec199bdfae4261ba6b57100
Staric <anton.staravoitau@gmail.com>
2016-12-05 15:30:14 +0300

    check comments

--- answers.js
+++ answers.js
@@ -1,44 +1,46 @@
 Array.prototype.first = function() {
     return this[0];
-}
+};
 Array.prototype.last = function() {
     return this[this.length - 1];
-}
+};
 Array.prototype.skip = function(start) {
     return this.slice(start);
-}
+};
 Array.prototype.take = function(end) {
     return this.slice(0, end);
-}
+};
 Array.prototype.while = function(predicate) {
     if (typeof predicate !== "function") {
         console.log("predicate must be a function");
-        return null;
+        return this;
     }
-    var newArray = [];
+    let newArray = [];
     for (let i = 0; i < this.length; i++) {
         if (predicate(this[i], i, this)) {
             newArray.push(this[i]);
+        } else {
+            break;
         }
     }
     return newArray;
-}
+};
 
 Array.prototype.toObject = function(predicate) {
     if (typeof predicate !== "function") {
         console.log("predicate must be a function");
-        return null;
+        return this;
     }
-    var newObject = {};
-    var tmpObject, tmpKey, tmpValue;
+    let newObject = {};
+    let tmpObject, tmpKey, tmpValue;
     for (let i = 0; i < this.length; i++) {
         tmpObject = predicate(this[i], i, this);
         newObject[tmpObject.key] = tmpObject.value;
     }
     return newObject;
-}
+};
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+let arr = [1, 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 2]
 let result = arr.while(function(elem, index, array) {
     return elem < 5;
 });
