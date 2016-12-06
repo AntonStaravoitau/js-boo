98e23dd5866e3f4641395cc2592f5022d36a5947
Staric <anton.staravoitau@gmail.com>
2016-12-06 15:37:13 +0300

    transform to module-style, check comments. I have made all my tasks for yesterday

--- answers.js
+++ answers.js
@@ -1,102 +1,166 @@
-Array.prototype.first = function() {
-    return this[0];
-};
+(function(module) {
+    module.first = function() {
+        return this[0];
+    };
 
-Array.prototype.last = function() {
-    return this[this.length - 1];
-};
+    module.last = function() {
+        return this[this.length - 1];
+    };
 
-Array.prototype.skip = function(start) {
-    return this.slice(start);
-};
+    module.skip = function(start) {
+        return this.slice(start);
+    };
 
-Array.prototype.take = function(end) {
-    return this.slice(0, end);
-};
+    module.take = function(end) {
+        return this.slice(0, end);
+    };
 
-Array.prototype.while = function(predicate) {
-    if (typeof predicate !== "function") {
-        console.log("predicate must be a function");
-        return this;
-    }
-    let newArray = [];
-    for (let i = 0; i < this.length; i++) {
-        if (predicate(this[i], i, this)) {
-            newArray.push(this[i]);
-        } else {
-            break;
+    function paramCheckDecorator(type, fn) {
+        return function(param) {
+            if (typeof param !== type) {
+                throw new Error('Predicate should be a function');
+                return this;
+            }
+
+            return fn.apply(this, arguments);
         }
     }
-    return newArray;
-};
 
-Array.prototype.filter = function(predicate) {
-    if (typeof predicate !== "function") {
-        console.log("predicate must be a function");
-        return this;
-    }
-    let newArray = [];
-    for (let i = 0; i < this.length; i++) {
-        if (predicate(this[i], i, this)) {
+    module.while = paramCheckDecorator('function', function(predicate) {
+        let newArray = [];
+
+        for (let i = 0; i < this.length; i++) {
+            if (predicate(this[i], i, this) === false) {
+                return newArray;
+            }
+
             newArray.push(this[i]);
         }
-    }
-    return newArray;
-};
 
-Array.prototype.toObject = function(predicate) {
-    if (typeof predicate !== "function") {
-        console.log("predicate must be a function");
-        return this;
-    }
-    let newObject = {};
-    let tmpObject, tmpKey, tmpValue;
-    for (let i = 0; i < this.length; i++) {
-        tmpObject = predicate(this[i], i, this);
-        newObject[tmpObject.key] = tmpObject.value;
-    }
-    return newObject;
-};
+        return newArray;
+    });
 
-Array.prototype.fold = function(predicate, initialValue) {
-    if (typeof predicate !== "function") {
-        console.log("predicate must be a function");
-        return this;
-    }
-    let previousValue, currentValue;
-    if (typeof initialValue === 'number' && initialValue === initialValue) {
+    module.filter = paramCheckDecorator('function', function(predicate) {
+
+        let newArray = [];
+        for (let i = 0; i < this.length; i++) {
+            if (predicate(this[i], i, this)) {
+                newArray.push(this[i]);
+            }
+        }
+        return newArray;
+    });
+
+    module.toObject = paramCheckDecorator('function', function(predicate) {
+
+        let newObject = {};
+
+        for (let i = 0; i < this.length; i++) {
+            let tmpObject = predicate(this[i], i, this);
+            newObject[tmpObject.key] = tmpObject.value;
+        }
+        return newObject;
+    });
+
+    module.fold = paramCheckDecorator('function', function(predicate, initialValue) {
+        if (typeof predicate !== "function") {
+            throw new Error('Predicate should be a function');
+            return this;
+        }
+        let previousValue;
         previousValue = this[0] + initialValue;
-    } else {
-        previousValue = this[0];
-    }
-    for (let i = 1; i < this.length; i++) {
-        previousValue = predicate(previousValue, this[i], i, this);
-    }
-    return previousValue;
-};
+        for (let i = 1; i < this.length; i++) {
+            previousValue = predicate(previousValue, this[i], i, this);
+        }
+        return previousValue;
+    });
 
-Array.prototype.transform = function(predicate) {
-    if (typeof predicate !== "function") {
-        console.log("predicate must be a function");
-        return this;
-    }
-    let newArray = [];
-    for (let i = 0; i < this.length; i++) {
-        newArray[i] = predicate(this[i], i, this);
-    }
-    return newArray;
-};
+    module.transform = paramCheckDecorator('function', function(predicate) {
 
-Array.prototype.each = function(predicate) {
-    if (typeof predicate !== "function") {
-        console.log("predicate must be a function");
+        let newArray = [];
+        for (let i = 0; i < this.length; i++) {
+            newArray.push(predicate(this[i], i, this));
+        }
+        return newArray;
+    });
+
+    module.each = paramCheckDecorator('function', function(predicate) {
+        for (let i = 0; i < this.length; i++) {
+            predicate(this[i], i, this);
+        }
         return this;
+    });
+
+    module.myFind = paramCheckDecorator('function', function(predicate) {
+
+        for (let i = 0; i < this.length; i++) {
+            if (predicate(this[i], i, this) === true) {
+                return this[i];
+            }
+        }
+
+        return udefined;
+    });
+
+    module.myFindLast = paramCheckDecorator('function', function(predicate) {
+
+        for (let i = this.length - 1; i >= 0; i--) {
+            if (predicate(this[i], i, this) === true) {
+                return this[i];
+            }
+        }
+
+        return udefined;
+    });
+
+    module.myFindIndex = paramCheckDecorator('function', function(predicate) {
+        for (let i = 0; i < this.length; i++) {
+            if (predicate(this[i], i, this) === true) {
+                return i;
+            }
+        }
+
+        return -1;
+    });
+
+    module.myFindIndexLast = paramCheckDecorator('function', function(predicate) {
+        for (let i = this.length - 1; i >= 0; i--) {
+            if (predicate(this[i], i, this) === true) {
+                return i;
+            }
+        }
+
+        return -1;
+    });
+
+})(Array.prototype);
+
+
+(function(module) {
+    function paramCheckDecorator(fn) {
+        return function() {
+            /*   if (typeof this !== 'object' && (typeof obj !== 'function' || obj === null)) {
+                   throw new TypeError('Object.keys called on non-object');
+                   return this;
+               }*/
+            return fn.apply(this, arguments);
+        };
     }
-    for (let i = 0; i < this.length; i++) {
-        predicate(this[i], i, this);
-    }
-    return this;
-};
+
+    module.myKeys = paramCheckDecorator(function() {
+        let hasOwnProperty = Object.prototype.hasOwnProperty;
+        let result = [],
+            prop;
+        for (prop in this) {
+            if (hasOwnProperty.call(this, prop)) {
+                result.push(prop);
+            }
+        }
+        return result;
+    });
+})(Object.prototype);
+
+
 
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 2]
@@ -117,7 +181,9 @@ let obj = massiveAttack.toObject(function(elem, index, array) {
     };
 });
 
-let accum = arr.fold(function(previousValue, currentValue, index, array) {
+
+
+let accum = arr.fold(function(previousValue, currentValue) {
     return previousValue + currentValue;
 }, 5);
 
@@ -138,3 +204,17 @@ console.log("fold: " + accum);
 console.log("transform: " + roots);
 console.log("each :");
 numbers.each(logArrayElements);
+console.log("find " + arr.myFind(function(elem, index, array) {
+    return elem < 5;
+}));
+console.log("findLast " + arr.myFindLast(function(elem, index, array) {
+    return elem < 5;
+}));
+console.log("findIndex: " + arr.myFindIndex(function(elem, index, array) {
+    return elem < 5;
+}));
+console.log("findIndexLast: " + arr.myFindIndexLast(function(elem, index, array) {
+    return elem < 5;
+}));
+
+console.log(obj.myKeys());
