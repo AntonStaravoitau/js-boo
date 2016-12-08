b267db0f7f14607ce8d95cc4015d1d46b217806b
Staric <anton.staravoitau@gmail.com>
2016-12-08 17:21:33 +0300

    tmp view code

--- answers2.js
+++ answers2.js
@@ -22,6 +22,14 @@ var Libra = {};
         return newObject;
     };
 
+
+    module.inherits = function(Child, Parent) {
+        let tmpObj = new Parent(5);
+        Child.prototype = tmpObj;
+        let newObj = new Child;
+        return newObj;
+    };
+
 })(Libra);
 
 let obj1 = {
@@ -40,5 +48,31 @@ let objResult2 = Libra.assign({}, obj1, obj2, {
     i: 1
 });
 
+
+function Figure(square) {
+    squre = square;
+}
+
+function Triangle() {
+    let angleCount = 3;
+}
+
+let triangle = new Libra.inherits(Triangle, Figure);
+
+
 console.log(objResult1); // { a: 1, b: 2, c: 3, d: 4}
 console.log(objResult2); // { a: 1, b: 2, c: 3, d: 4, o: 30, i: 1 }
+console.log(triangle instanceof Figure)
+
+/*
+function Animal() {
+    eats: true;
+};
+let animal = new Animal();
+function Rabbit(name) {
+    this.name = name;
+    this.__proto__ = animal;
+}
+var rabbit = new Rabbit("Кроль");
+//alert( rabbit instanceof Animal ); // true, из прототипа
+    */
